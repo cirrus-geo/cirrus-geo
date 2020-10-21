01659e0d40b4fa1287fd5aed192a85e571548340
Matthew Hanson <matt.a.hanson@gmail.com>
2020-10-21 19:12:07 -0400

    update publish logging

--- tasks/publish/lambda_function.py
+++ tasks/publish/lambda_function.py
@@ -8,11 +8,6 @@ from cirruslib import Catalog, Catalogs, StateDB
 from cirruslib.transfer import get_s3_session
 from logging import getLogger
 from os import getenv
-from traceback import format_exc
-
-# configure logger - CRITICAL, ERROR, WARNING, INFO, DEBUG
-logger = getLogger(__name__)
-logger.setLevel(getenv('CIRRUS_LOG_LEVEL', 'INFO'))
 
 # envvars
 DATA_BUCKET = getenv('CIRRUS_DATA_BUCKET')
@@ -24,11 +19,7 @@ statedb = StateDB()
 
 
 def lambda_handler(payload, context):
-    logger.debug('Payload: %s' % json.dumps(payload))
-
     catalog = Catalogs.from_payload(payload)[0]
-
-    logger.debug('Catalog: %s' % json.dumps(catalog))
 
     config = catalog['process']['tasks'].get('publish', {})
     public = config.get('public', False)
@@ -39,6 +30,8 @@ def lambda_handler(payload, context):
     s3urls = []
 
     try:
+        catalog.logger.debug("Publishing items to s3 and SNS")
+
         # publish to s3
         s3urls = catalog.publish_to_s3(DATA_BUCKET, public=public)
 
@@ -53,19 +46,16 @@ def lambda_handler(payload, context):
         for t in topics:
             catalog.publish_to_sns(t)
     except Exception as err:
-        msg = f"publish: failed publishing output items in {catalog['id']} ({err})"
-        logger.error(msg)
-        logger.error(format_exc())
+        msg = f"publish: failed publishing output items ({err})"
+        catalog.logger.error(msg, exc_info=True)
         raise Exception(msg) from err
 
     try:
         # update processing in table
         statedb.set_completed(catalog['id'], s3urls)
-        logger.info(f"publish: completed processing {catalog['id']}")
     except Exception as err:
-        msg = f"publish: failed setting {catalog['id']} as complete ({err})"
-        logger.error(msg)
-        logger.error(format_exc())
+        msg = f"publish: failed setting as complete ({err})"
+        catalog.logger.error(msg, exc_info=True)
         raise Exception(msg) from err        
 
     return catalog
