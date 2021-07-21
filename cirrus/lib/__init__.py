06df65f947c786a7660a89bfbad1be2ca744c889
jkeifer <jkeifer@element84.com>
2021-07-21 16:38:13 -0700

    clean up a couple things in cirrus.lib

--- cirrus/lib/__init__.py
+++
@@ -1,4 +0,0 @@
-from cirrus.lib.catalog import Catalog, Catalogs
-from cirrus.lib.logging import get_task_logger
-from cirrus.lib.statedb import StateDB, STATES
-from cirrus.lib.version import __version__
