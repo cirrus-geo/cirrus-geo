f1a5ec7e699ff7850c175b19fa0f990b69dccb65
jkeifer <jkeifer@element84.com>
2021-09-15 22:26:06 -0700

    task job definitions inherit env vars if not set

--- cirrus/cli/components/tasks/__init__.py
+++ cirrus/cli/components/tasks/__init__.py
@@ -6,15 +6,59 @@ from ..base import Lambda
 from cirrus.cli.resources import TaskResource
 
 
+JOB_DEFINITION_TYPE = 'AWS::Batch::JobDefinition'
+
+
+def convert_lambda_env_to_batch_env(env):
+    for name, val in env.items():
+        yield {'Name': name, 'Value': val}
+
+
+def set_nested_value_if_unset(_dict, value, *keys):
+    for key in keys[:-1]:
+        if key not in _dict:
+            _dict[key] = {}
+        elif not hasattr(_dict[key], '__getitem__'):
+            raise RuntimeError(
+                f"Key {key} already set on object and value not dict-like",
+            )
+        _dict = _dict[key]
+
+    key = keys[-1]
+    if key not in _dict:
+        _dict[key] = value
+
+
 class Task(Lambda):
     def load_config(self):
         super().load_config()
+        self.batch_env = list(convert_lambda_env_to_batch_env(
+            self.config.get('environment', {}),
+        ))
         batch = self.config.pop('batch', {})
         self.batch_enabled = batch.get('Enabled', False)
         self.batch_resources = [
-            TaskResource(self, name, definition, self.definition.path)
+            self.create_batch_resource(name, definition)
             for name, definition in batch.get('Resources', {}).items()
         ]
+
+    def create_batch_resource(self, name, definition):
+        resource = TaskResource(self, name, definition, self.definition.path)
+
+        if resource.type != JOB_DEFINITION_TYPE:
+            return resource
+
+        # load env into JobDefn.Properties.ContainerProperties.Environment
+        if self.batch_env:
+            set_nested_value_if_unset(
+                resource.definition,
+                self.batch_env,
+                'Properties',
+                'ContainerProperties',
+                'Environment',
+            )
+
+        return resource
 
     def detail_display(self):
         super().detail_display()
