de4c17b667ff1040398836a0a5fafc8a31c6091b
jkeifer <jkeifer@element84.com>
2021-09-24 08:02:46 -0700

    workflow-failed invalid SNS topic support

--- cirrus/cli/components/tasks/config/workflow-failed/handler.py
+++ cirrus/cli/components/tasks/config/workflow-failed/handler.py
@@ -6,6 +6,7 @@ from cirruslib import Catalog, StateDB, get_task_logger
 
 # envvars
 FAILED_TOPIC_ARN = getenv('CIRRUS_FAILED_TOPIC_ARN', None)
+INVALID_TOPIC_ARN = getenv('CIRRUS_INVALID_TOPIC_ARN', None)
 
 # boto3 clients
 SNS_CLIENT = boto3.client('sns')
@@ -31,7 +32,7 @@ def get_error_from_batch(logname):
 
 def handler(payload, context):
     catalog = Catalog.from_payload(payload)
-    logger = get_task_logger(f"{__name__}.workflow-failed", catalog=catalog)
+    logger = get_task_logger("task.workflow-failed", catalog=catalog)
 
     # parse errors
     error = payload.get('error', {})
@@ -64,14 +65,16 @@ def handler(payload, context):
     try:
         if error_type == "InvalidInput":
             statedb.set_invalid(catalog['id'], error)
+            notification_topic_arn = INVALID_TOPIC_ARN
         else:
             statedb.set_failed(catalog['id'], error)
+            notification_topic_arn = FAILED_TOPIC_ARN
     except Exception as err:
         msg = f"Failed marking as failed: {err}"
         logger.error(msg, exc_info=True)
         raise err
 
-    if FAILED_TOPIC_ARN is not None:
+    if notification_topic_arn is not None:
         try:
             item = statedb.dbitem_to_item(statedb.get_dbitem(catalog['id']))
             attrs = {
@@ -88,11 +91,15 @@ def handler(payload, context):
                     'StringValue': error
                 }
             }
-            logger.debug(f"Publishing item to {FAILED_TOPIC_ARN}")
-            SNS_CLIENT.publish(TopicArn=FAILED_TOPIC_ARN, Message=json.dumps(item), MessageAttributes=attrs)
+            logger.debug(f"Publishing item to {notification_topic_arn}")
+            SNS_CLIENT.publish(
+                TopicArn=notification_topic_arn,
+                Message=json.dumps(item),
+                MessageAttributes=attrs,
+            )
         except Exception as err:
-            msg = f"Failed publishing to {FAILED_TOPIC_ARN}: {err}"
+            msg = f"Failed publishing to {notification_topic_arn}: {err}"
             logger.error(msg, exc_info=True)
             raise err
-    
+
     return catalog
