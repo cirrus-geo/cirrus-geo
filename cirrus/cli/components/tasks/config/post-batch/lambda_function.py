ae216a4f990426abd806f0fda71a15957473904f
Jarrett Keifer <jkeifer@users.noreply.github.com>
2021-12-22 09:39:18 -0800

    post-batch cleanup and fixes re 75 (#86)
    * post-batch cleanup and fixes re 75
    
    - Ensures exceptions within the function are logged
    - No longer drops chars at beginning of error messages:
        - Uses `removeprefix` as intended, instead of `lstrip`
    - More robust error class determination:
        - Now matches on whole word at start of message text ending with colon
        - Still susceptiable to messages like `Error:.*` or the like
    - Other cleanup

--- cirrus/cli/components/tasks/config/post-batch/lambda_function.py
+++ cirrus/cli/components/tasks/config/post-batch/lambda_function.py
@@ -1,36 +1,47 @@
-import boto3
+import re
 import json
+
+import boto3
+
 from cirruslib import Catalog
+from cirruslib.logging import get_task_logger
 
 
+logger = get_task_logger('lambda_function.update-state', catalog=tuple())
+
+BATCH_LOG_GROUP = '/aws/batch/job'
 LOG_CLIENT = boto3.client('logs')
-
-
-class BaseException(Exception):
-    pass
+DEFAULT_ERROR = 'UnknownError'
+ERROR_REGEX = re.compile(r'^(?:cirrus\.?lib\.errors\.)?(?:([\.\w]+):)?\s*(.*)')
 
 
 def lambda_handler(payload, context):
-    if 'error' in payload:
-        error = payload.get('error', {})
-        cause = json.loads(error['Cause'])
-        logname = cause['Attempts'][-1]['Container']['LogStreamName']
+    if 'error' not in payload:
+        catalog = Catalog.from_payload(payload)
+        return catalog
+
+    error = payload.get('error', {})
+    cause = json.loads(error['Cause'])
+    logname = cause['Attempts'][-1]['Container']['LogStreamName']
+
+    try:
         error_type, error_msg = get_error_from_batch(logname)
-        exception_class = type(error_type, (BaseException, Exception), {})
-        raise exception_class(error_msg)
-    catalog = Catalog.from_payload(payload)
-    return catalog
+    except Exception:
+        # lambda does not currently support exeception chaining,
+        # so we have to log the original exception separately
+        logger.exception("Original exception:")
+        raise Exception("Unable to get error log")
+
+    exception_class = type(error_type, (Exception,), {})
+    raise exception_class(error_msg)
 
 
 def get_error_from_batch(logname):
-    try:
-        logs = LOG_CLIENT.get_log_events(logGroupName='/aws/batch/job', logStreamName=logname)
-        msg = logs['events'][-1]['message'].lstrip('cirruslib.errors.')
-        parts = msg.split(':', maxsplit=1)
-        if len(parts) > 1:
-            error_type = parts[0]
-            msg = parts[1]
-            return error_type, msg
-        return "Unknown", msg
-    except Exception:
-        return "Exception", "Unable to get Error Log"
+    logger.info('Getting error from %s/%s', BATCH_LOG_GROUP, logname)
+    logs = LOG_CLIENT.get_log_events(
+        logGroupName=BATCH_LOG_GROUP,
+        logStreamName=logname,
+    )
+    msg = logs['events'][-1]['message']
+    error_type, msg = ERROR_REGEX.match(msg).groups()
+    return error_type or DEFAULT_ERROR, msg
