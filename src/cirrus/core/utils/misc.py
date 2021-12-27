47007bcbdb0b4b6cd774f03ae61d83902be51508
jkeifer <jkeifer@element84.com>
2021-12-27 15:10:17 -0800

    remove errant print calls

--- src/cirrus/core/utils/misc.py
+++ src/cirrus/core/utils/misc.py
@@ -10,10 +10,8 @@ def get_cirrus_lib_requirements() -> List[str]:
     '''
     try:
         from importlib import metadata
-        print("using importlib")
     except ImportError:
         import importlib_metadata as metadata
-        print("using importlib_metadata")
 
     return [
         req.split(';')[0].translate(str.maketrans('','',' ()'))
