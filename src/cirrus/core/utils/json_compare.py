13739fd288765ab30a7e9b12dbe86f6d5f3d8f94
jkeifer <jkeifer@element84.com>
2022-01-11 17:12:32 -0800

    recursive_compare ported to cirrus-lib

--- src/cirrus/core/utils/json_compare.py
+++ src/cirrus/core/utils/json_compare.py
@@ -1,27 +1,6 @@
-def recursive_compare(d1, d2, level='root'):
-    if isinstance(d1, dict) and isinstance(d2, dict):
-        if d1.keys() != d2.keys():
-            s1 = set(d1.keys())
-            s2 = set(d2.keys())
-            print('{:<20} + {} - {}'.format(level, s1-s2, s2-s1))
-            common_keys = s1 & s2
-        else:
-            common_keys = set(d1.keys())
+import sys
 
-        for k in common_keys:
-            recursive_compare(d1[k], d2[k], level='{}.{}'.format(level, k))
-
-    elif isinstance(d1, list) and isinstance(d2, list):
-        if len(d1) != len(d2):
-            print('{:<20} len1={}; len2={}'.format(level, len(d1), len(d2)))
-        common_len = min(len(d1), len(d2))
-
-        for i in range(common_len):
-            recursive_compare(d1[i], d2[i], level='{}[{}]'.format(level, i))
-
-    else:
-        if d1 != d2:
-            print('{:<20} {} != {}'.format(level, d1, d2))
+from cirrus.lib import utils
 
 
 def parse_args(args=None):
@@ -45,4 +24,4 @@ if __name__ == '__main__':
     d1 = load_json(args.json1_file)
     d2 = load_json(args.json2_file)
 
-    recursive_compare(d1, d2)
+    sys.exit(not utils.recursive_compare(d1, d2))
