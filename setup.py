4e419e4274446b15b69b9efc0fc5cbfcaddc4472
jkeifer <jkeifer@element84.com>
2022-05-31 15:34:37 -0700

    set version from last tag when installing from local dir

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import os.path
+import subprocess
 
 from setuptools import setup, find_namespace_packages
 
@@ -8,7 +9,50 @@ from src.cirrus.cli.constants import DESC
 
 
 HERE = os.path.abspath(os.path.dirname(__file__))
-VERSION = os.environ.get('CIRRUS_VERSION', '0.0.0')
+
+
+# gets the version from the latest tag via git describe
+# so we don't have to do anything to manage version number
+# aside from tagging releases
+def git_version(gitdir, default='0.0.0'):
+    try:
+        desc = subprocess.run(
+            [
+                'git',
+                '--git-dir',
+                gitdir,
+                'describe',
+                '--long',
+                '--tags',
+                '--dirty',
+            ],
+            capture_output=True,
+        )
+    except Exception:
+        return default
+
+    if desc.returncode != 0:
+        return default
+
+    # example output: v0.5.1-8-gb38722d-dirty
+    # parts are:
+    #  0 - last tag
+    #  1 - commits since last tag (0 if same commit as tag)
+    #  2 - short hash of current commit
+    #  3 - dirty (if repo state is dirty)
+    parts = desc.stdout.decode().strip().lstrip('v').split('-', maxsplit=2)
+    if int(parts[1]) > 0 or 'dirty' in parts[2]:
+        return f'{parts[0]}+{parts[1]}.{parts[2].replace("-",".")}'
+    else:
+        return parts[0]
+
+
+# in the case of a tagged release, we
+# are passed a version in an env var
+VERSION = os.environ.get(
+    'CIRRUS_VERSION',
+    git_version(os.path.join(HERE, '.git')),
+)
 
 
 with open(os.path.join(HERE, 'README.md'), encoding='utf-8') as f:
