9796e772218ddf0a35c7f1ad1074079c5eb21ec8
jkeifer <jkeifer@element84.com>
2022-08-24 08:48:43 -0700

    support for serverless version upgrade re #139

--- src/cirrus/core/constants.py
+++ src/cirrus/core/constants.py
@@ -2,10 +2,9 @@ DEFAULT_CONFIG_FILENAME = 'cirrus.yml'
 DEFAULT_SERVERLESS_FILENAME = 'serverless.yml'
 DEFAULT_BUILD_DIR_NAME = '.cirrus'
 
-SERVERLESS = {'serverless': '^1.83.1'}
+SERVERLESS = {'serverless': '~3.18.0'}
 SERVERLESS_PLUGINS = {
-    'serverless-python-requirements': '~5.1.0',
-    'serverless-step-functions': '~2.27.1',
-    'serverless-pseudo-parameters': '~2.5.0',
-    'serverless-iam-roles-per-function': '~3.1.0',
+    'serverless-python-requirements': '~5.4.0',
+    'serverless-step-functions': '~3.7.0',
+    'serverless-iam-roles-per-function': '~3.2.0',
   }
