e4a612b05c8ea9a4f2e15372f8333f36a5525c55
Phil Varner <pvarner@element84.com>
2024-01-10 15:08:21 -0500

    handle errors with batch task container fails

--- src/cirrus/builtins/tasks/post-batch/lambda_function.py
+++ src/cirrus/builtins/tasks/post-batch/lambda_function.py
@@ -24,29 +24,52 @@ def lambda_handler(event: dict[str, Any], context: Any) -> dict[str, Any]:
         logger.exception(f"Original error: {json.dumps(error)}")
         raise Exception("Unable to get error log: Cause is not defined")
 
-    cause = json.loads(error.get("Cause", {}))
+    cause = json.loads(error.get("Cause", "{}"))
     attempts = cause.get("Attempts", [])
     if not attempts:
         logger.exception(f"Original error: {json.dumps(error)}")
         raise Exception("Unable to get error log: Attempts is empty")
 
-    logname = attempts[-1].get("Container", {}).get("LogStreamName")
+    attempt = attempts[-1]
+    container = attempt.get("Container")
+    if container is None:
+        logger.exception(f"Original error: {json.dumps(error)}")
+        raise Exception(
+            "Unable to get error log: Container for last Attempt is missing"
+        )
+
+    logname = container.get("LogStreamName")
     if not logname:
         logger.exception(f"Original error: {json.dumps(error)}")
         raise Exception(
-            "Unable to get error log: Container or LogStreamName for last Attempt is missing"
+            "Unable to get error log: LogStreamName for last Attempt is missing"
         )
 
+    error_from_batch = None
     try:
-        error_type, error_msg = get_error_from_batch(logname)
+        error_from_batch = get_error_from_batch(logname)
     except Exception as e:
         # lambda does not currently support exeception chaining,
         # so we have to log the original exception separately
-        logger.exception(f"Original error: {json.dumps(error)}")
-        raise Exception(f"Unable to get error log '{BATCH_LOG_GROUP}/{logname}': {e}")
+        logger.exception(
+            f"Unable to get error log '{BATCH_LOG_GROUP}/{logname}' because {e}, original error: {json.dumps(error)}"
+        )
 
-    exception_class = type(error_type, (Exception,), {})
-    raise exception_class(error_msg)
+    if error_from_batch:
+        error_type, error_msg = error_from_batch
+        exception_class = type(error_type, (Exception,), {})
+        raise exception_class(error_msg)
+    else:
+        # if the cloudwatch error log cannot be retrieved, it's likely that the container
+        # didn't start and nothing was logged anyway, so we log the reason for that instead
+        container_reason = container.get(
+            "Reason"
+        )  # e.g., "DockerTimeoutError: Could not transition to created; timed out after waiting 4m0s"
+        status_reason = attempt.get("StatusReason")  # e.g., "Task failed to start"
+        raise Exception(
+            "Unable to get error log, container likely never ran. "
+            f"Container Reason: {container_reason}; Status Reason: {status_reason}"
+        )
 
 
 def get_error_from_batch(logname: str) -> Optional[Tuple[str, str]]:
