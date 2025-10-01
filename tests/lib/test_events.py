863149427014699c252672e0bbd48041184cb1db
Ian Cooke <icooke@element84.com>
2025-10-01 08:29:47 -0400

    test: monkeypatch to avoid polluting environment
    fix: enable/disable logic error

--- tests/lib/test_events.py
+++ tests/lib/test_events.py
@@ -26,10 +26,10 @@ def make_event():
 
 
 @mock_aws
-def test_workflow_metric_logger_and_and_send():
+def test_workflow_metric_logger_and_and_send(monkeypatch):
     log_group_name = "ircwaves-test-20250903"
     # Setup environment variables for log group and metric name
-    os.environ["CIRRUS_WORKFLOW_LOG_GROUP"] = log_group_name
+    monkeypatch.setenv("CIRRUS_WORKFLOW_LOG_GROUP", log_group_name)
 
     # Create log group and log stream using boto3 client
     logs_client = get_client("logs")
@@ -50,6 +50,14 @@ def test_workflow_metric_logger_and_and_send():
     assert len(events["events"]) == 2
 
 
+@mock_aws
+def test_workflow_metric_logger_disabled(monkeypatch):
+    monkeypatch.delenv("CIRRUS_WORKFLOW_LOG_GROUP", raising=False)
+
+    metric_logger = WorkflowMetricReader()
+    assert not metric_logger.enabled()
+
+
 @pytest.mark.skip(reason="moto does not support CloudWatch Log Metric Filters")
 @mock_aws
 def test_workflow_metric_reader_get_statistics():
@@ -65,6 +73,8 @@ def test_workflow_metric_reader_get_statistics():
         '
 
     Note: this test will create a log group and metric filters, if they do not exist.
+
+    Also, this test would use `monkeypatch.setenv`, if it were runnable in pytest-mode.
     """
     log_group_name = "cirrus-deployment"
 
