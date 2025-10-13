b6f94cccf780f6559ca7d37b67af488874f2f556
jkeifer <jkeifer@element84.com>
2025-10-13 13:27:35 -0700

    fix template directory inclusion

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -44,7 +44,7 @@ source_suffix = {
 }
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+templates_path = ["templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
